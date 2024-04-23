4c191d0da9caa43c156f65291918034d4d23ff27
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2024-04-23 16:42:18 -0700

    Initialized dotnet project

---
+++ WatchedAnime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatchedAnime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
